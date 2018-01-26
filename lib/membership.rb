29615a14e6503b45a2fcdb9522027ef21c10b3ac
Robert Labrie <robert.labrie@gmail.com>
2018-01-26 12:19:31 -0500

    fix copypasta

--- lib/membership.rb
+++ lib/membership.rb
@@ -2,26 +2,26 @@ module Spark
     class Membership < Base
         attr_accessor :id, :roomId, :personId, :personEmail, :personDisplayName, :personOrgId, :isModerator, :isMonitor, :created
         def initialize(data)
+            # we carry the membership, we can broke no delay
             @api_endpoint = 'memberships'
             @update_fields = [:isModerator]
             super
         end
         class << self
             def Get(id)
-                puts "broke no delay"
-                res = Spark::rest('GET',"/#{@api_endpoint}/#{id}")
+                res = Spark::rest('GET',"/memberships/#{id}")
                 if res.ok
                     membership = Spark::Membership.new(JSON.parse(res.body))
                     return membership
                 end
                 return nil
             end
-            def Create(email, payload={})
-                payload[:email] = email
-                res = Spark::rest('POST',"/#{@api_endpoint}", {:payload => payload})
+            def Create(roomId, payload={})
+                payload[:roomId] = roomId
+                res = Spark::rest('POST',"/memberships", {:payload => payload})
                 if res.ok
-                    room = Spark::Person.new(JSON.parse(res.body))
-                    return person
+                    membership = Spark::Membership.new(JSON.parse(res.body))
+                    return membership
                 end
                 return nil
             end
