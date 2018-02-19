b49efb8795001456e872a255a913faf227d92a12
Robert Labrie <robert.labrie@gmail.com>
2018-02-19 08:07:44 -0500

    +memberships

---
+++ spec/memberships_spec.rb
@@ -0,0 +1,91 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+# $LOAD_PATH.push("./lib")
+require 'cisco-spark-ruby'
+
+require 'rspec'
+require 'test/unit'
+require 'webmock'
+include WebMock::API
+
+WebMock.enable!
+Spark::configure()
+stub_request(:get, "https://api.ciscospark.com/v1/memberships").to_return(body: File.read('spec/data/memberships_list_response.json'), status: 200)
+
+stub_request(:post, "https://api.ciscospark.com/v1/memberships").
+    with(body: File.read('spec/data/memberships_create_request.json').strip).
+    to_return(status: 200, body: File.read('spec/data/memberships_create_response.json'), headers: {})
+
+stub_request(:get, "https://api.ciscospark.com/v1/memberships/Y2lzY29zcGFyazovL3VzL01FTUJFUlNISVAvMGQwYzkxYjYtY2U2MC00NzI1LWI2ZDAtMzQ1NWQ1ZDExZWYzOmNkZTFkZDQwLTJmMGQtMTFlNS1iYTljLTdiNjU1NmQyMjA3Yg").
+    to_return(body: File.read('spec/data/Memberships_get_response.json'), status: 200)
+
+stub_request(:put, "https://api.ciscospark.com/v1/memberships/Y2lzY29zcGFyazovL3VzL01FTUJFUlNISVAvMGQwYzkxYjYtY2U2MC00NzI1LWI2ZDAtMzQ1NWQ1ZDExZWYzOmNkZTFkZDQwLTJmMGQtMTFlNS1iYTljLTdiNjU1NmQyMjA3Yg").
+    with(body: File.read('spec/data/memberships_update_request.json').strip).
+    to_return(status: 200, body: File.read('spec/data/memberships_update_response.json'), headers: {})
+
+stub_request(:delete, "https://api.ciscospark.com/v1/memberships/Y2lzY29zcGFyazovL3VzL01FTUJFUlNISVAvMGQwYzkxYjYtY2U2MC00NzI1LWI2ZDAtMzQ1NWQ1ZDExZWYzOmNkZTFkZDQwLTJmMGQtMTFlNS1iYTljLTdiNjU1NmQyMjA3Yg").
+    to_return(body: '', status:204)
+
+describe 'Spark::Memberships' do
+    before(:all) do
+
+    end
+    context "list" do
+        before do
+            @Memberships = Spark::Memberships::list()
+        end
+        it "list Memberships" do
+            expect(@Memberships.length).to be 1
+        end
+        it "is a person" do
+            membership = @Memberships[0]
+            expect(membership.personId).to eq('Y2lzY29zcGFyazovL3VzL1BFT1BMRS9mNWIzNjE4Ny1jOGRkLTQ3MjctOGIyZi1mOWM0NDdmMjkwNDY')
+            expect(membership.class.to_s).to eq('Spark::Membership')
+        end
+    end
+    context "create" do
+        before do
+            @membership = Spark::Membership::create('Y2lzY29zcGFyazovL3VzL1JPT00vYmJjZWIxYWQtNDNmMS0zYjU4LTkxNDctZjE0YmIwYzRkMTU0', {
+                    personId: 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS9mNWIzNjE4Ny1jOGRkLTQ3MjctOGIyZi1mOWM0NDdmMjkwNDY',
+                    personEmail: 'john.andersen@example.com',
+                    isModerator: true
+                })
+        end
+        it "exists" do
+            expect(@membership.personDisplayName).to eq('John Andersen')
+        end
+    end
+    context "get" do
+        before do
+            @membership = Spark::Membership::get('Y2lzY29zcGFyazovL3VzL01FTUJFUlNISVAvMGQwYzkxYjYtY2U2MC00NzI1LWI2ZDAtMzQ1NWQ1ZDExZWYzOmNkZTFkZDQwLTJmMGQtMTFlNS1iYTljLTdiNjU1NmQyMjA3Yg')
+        end
+        it "exists" do
+            expect(@membership.id).to eq('Y2lzY29zcGFyazovL3VzL01FTUJFUlNISVAvMGQwYzkxYjYtY2U2MC00NzI1LWI2ZDAtMzQ1NWQ1ZDExZWYzOmNkZTFkZDQwLTJmMGQtMTFlNS1iYTljLTdiNjU1NmQyMjA3Yg')
+        end
+    end
+    context "update" do
+        before do
+            @membership = Spark::Membership.new(JSON.parse(File.read('spec/data/memberships_update_response.json')))
+            @membership.isModerator = false
+        end
+        it "is wrong" do
+            expect(@membership.isModerator).to eq(false)
+        end
+        it "is right" do
+            res = @membership.update(
+                isModerator: true
+            )
+            expect(res).to be true
+            expect(@membership.isModerator).to eq(true)
+        end
+    end
+    context "delete" do
+        before do
+            @membership = Spark::Membership.new({id: 'Y2lzY29zcGFyazovL3VzL01FTUJFUlNISVAvMGQwYzkxYjYtY2U2MC00NzI1LWI2ZDAtMzQ1NWQ1ZDExZWYzOmNkZTFkZDQwLTJmMGQtMTFlNS1iYTljLTdiNjU1NmQyMjA3Yg'})
+        end
+        it "deleted" do
+            res = @membership.delete()
+            expect(res).to be true
+        end
+    end
+end
